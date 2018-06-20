6c7016536b755c6d3ab973ebb6a15ba52d0f4672
Jason Ibrahim <jibrahim@opentext.com>
2018-06-20 14:52:34 -0700

    empty player with method sigs

--- src/Player.js
+++ src/Player.js
@@ -3,7 +3,41 @@ const lame = require('lame');
 const Speaker = require('speaker');
 
 class Player {
-  // TODO: implement me
+  
+  constructor() {
+  }
+  
+  load(file) {
+    // TODO: implement me
+  }
+  
+  play() {
+    // TODO: implement me
+  }
+  
+  pause() {
+    // TODO: implement me
+  }
+  
+  reset() {
+    // TODO: implement me
+  }
+  
+  isPlaying() {
+    // TODO: implement me
+  }
+  
+  getCurrentAudioStream() {
+    // TODO: implement me
+  }
+  
+  getCurrentSpeaker() {
+    // TODO: implement me
+  }
+  
+  getCurrentDecoder() {
+    // TODO: implement me
+  }
 }
 
 module.exports = Player;
